2f26946ee1d4784ca0693262ec851cf6729fb8dd
Nikin Nagewadia <nikin.n@gmail.com>
2020-11-29 19:30:57 +0000

    Reapproach to obtaining and displaying data

--- tweepy_Trends.py
+++ tweepy_Trends.py
@@ -1,27 +1,24 @@
 import tweepy
-import random
 import twitter_credentials
+import random
+import time, threading
 
 auth = tweepy.OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_SECRET)
 auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
 api = tweepy.API(auth)
-
 locations = api.trends_available()
 
-woeid_list = []
+location_list = []
 
 for location in locations:
-    # location_name = location['name']
-    woeid = location['woeid']
-    woeid_list.append(woeid)
-    # print(location_name)
-    # print(woeid)
+    location_name = location['name']
+    location_list.append(location_name)
 
-random_woeid = random.choice(woeid_list)
+WAIT_SECONDS = 1
 
-trends = api.trends_place(id=random_woeid)
+def locationOutput():
+    random_location = random.choice(location_list)
+    print(random_location)
+    threading.Timer(WAIT_SECONDS, locationOutput).start()
 
-for value in trends: 
-    for trend in value['trends']: 
-        print(trend['name'])
+locationOutput()
