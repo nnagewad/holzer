f5d5c3136e6e652d71de0eccd804bbb6ad637c3b
Nikin Nagewadia <nikin.n@gmail.com>
2020-12-02 22:47:12 +0000

    Get corresponding location and woeid

--- tweepy_Trends.py
+++ tweepy_Trends.py
@@ -6,23 +6,27 @@ import time, threading
 auth = tweepy.OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_SECRET)
 auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+
 locations = api.trends_available()
 
-woeid_list = []
 location_list = []
 
 for location in locations:
-    woeid_number = location['woeid']
     city_name = location['name']
     country_name = location['country']
-    woeid_list.append(woeid_number)
-    location_list.append(city_name + ", " + country_name)
+    woeid_number = location['woeid']
+    if city_name == country_name:
+        location_list.append(country_name + "woeid: " + str(woeid_number))
+    else:
+        location_list.append(city_name + ", " + country_name + "woeid: " + str(woeid_number))
 
 WAIT_SECONDS = 1
 
-def locationOutput():
+def locationFunction():
     selected_location = random.choice(location_list)
-    print(selected_location)
-    threading.Timer(WAIT_SECONDS, locationOutput).start()
+    locationArray = selected_location.rsplit("woeid: ")
+    print(locationArray[0])
+    print(locationArray[1])
+    threading.Timer(WAIT_SECONDS, locationFunction).start()
 
-locationOutput()
+locationFunction()
