defc8e87b872c193d207909e8f35bce364425d80
Nikin Nagewadia <nikin.n@gmail.com>
2020-12-06 15:54:37 +0000

    break into smaller files

--- app.py
+++ app.py
@@ -1,34 +1,11 @@
-import tweepy
-import twitter_credentials
-
-auth = tweepy.OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_SECRET)
-auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
-locations = api.trends_available()
-country_list = []
-city_list = []
-
-for location in locations:
-    city_name = location['name']
-    country_name = location['country']
-    city_list.append(city_name)
-    country_list.append(country_name)
-
-country_list = list(dict.fromkeys(country_list))
-city_list = set(city_list) - set(country_list)
-city_list = list(city_list)
-
-country_list.remove('')
-city_list.remove('Worldwide')
-
+import locations
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',cityList=city_list,countryList=country_list)
+    return render_template('index.html',cityList=locations.city_list,countryList=locations.country_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ locations.py
@@ -0,0 +1,23 @@
+import tweepy
+import twitter_credentials
+
+auth = tweepy.OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_SECRET)
+auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+locations = api.trends_available()
+country_list = []
+city_list = []
+
+for location in locations:
+    city_name = location['name']
+    country_name = location['country']
+    city_list.append(city_name)
+    country_list.append(country_name)
+
+country_list = list(dict.fromkeys(country_list))
+city_list = set(city_list) - set(country_list)
+city_list = list(city_list)
+
+country_list.remove('')
+city_list.remove('Worldwide')
